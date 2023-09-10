0255ccef2e074923e6283fa1221129c3121e5860
Jaimilit <>
2023-09-10 13:01:21 +0000

    now you can click and unclick box colors

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
     $(".box").on("click", function () {
         var classNames = $(this).attr("class").split(" ");
-        $("." + classNames[1]).css("background-color", "red");
+        var boxClass = classNames[0];
+        var className = classNames[1]; // Use className, not classNames
+
+        if ($(this).css("background-color") === "rgb(255, 0, 0)") { // Remove the space
+            $("." + className).css("background-color", "#000"); // Use className, not className[1]
+        } else {
+            $("." + boxClass).css("background-color", "#000");
+            $("." + className).css("background-color", "red");
+        }
     });
 });
